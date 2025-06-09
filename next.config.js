9083d6983e8994b63f6b7e08180507145f1310ad
Keiju Hirano <keijuhirano753plus9@gmail.com>
2025-06-10 00:43:11 +0900

    yml8

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  };
+  
+  env: {
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
+  
+};
   
   module.exports = nextConfig;
   
